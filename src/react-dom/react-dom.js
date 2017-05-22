82ed83ad2379bee6c200ca4f9334fb7a516b1d74
blackmiaool <blackmiaool@gmail.com>
2017-05-22 22:12:13 +0800

    remote log

--- src/react-dom/react-dom.js
+++ src/react-dom/react-dom.js
@@ -1080,7 +1080,6 @@ function render(element, target) {
 }
 
 function findDOMNode(component) {
-    console.log('component', this, component);
     if (component instanceof Node) {
         return component;
     }
