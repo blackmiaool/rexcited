564d93f6fffd6889be640a28cd74d928f84a7296
blackmiaool <blackmiaool@gmail.com>
2017-05-22 20:46:39 +0800

    update test case

--- test/index.js
+++ test/index.js
@@ -1,15 +1,14 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './components/App'
-import reducer from './reducers'
+class HelloMessage extends React.Component {
+    render() {
+        return React.createElement(
+            "div",
+            null,
+            "Hello ",
+            this.props.name
+        );
+    }
+}
 
-const store = createStore(reducer)
-
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+ReactDOM.render(React.createElement(HelloMessage, {
+    name: "Jane"
+}), document.getElementById('root'));
