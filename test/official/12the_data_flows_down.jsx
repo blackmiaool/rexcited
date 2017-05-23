28a8d0fa628b31b8b87d9642bf9fe5af5ddd9b38
blackmiaool <blackmiaool@gmail.com>
2017-05-23 18:37:37 +0800

    add test cases

---
+++ test/official/12the_data_flows_down.jsx
@@ -0,0 +1,50 @@
+function FormattedDate(props) {
+    return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
+}
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date()
+        };
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+        <h1>Hello, world!</h1>
+        <FormattedDate date={this.state.date} />
+      </div>
+        );
+    }
+}
+
+function App() {
+    return (
+        <div>
+      <Clock />
+      <Clock />
+      <Clock />
+    </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
