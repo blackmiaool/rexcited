afe2bc844623a5c252c861baa0d747c077fbc605
blackmiaool <blackmiaool@gmail.com>
2017-05-29 14:32:42 +0800

    update demo

--- test/index.js
+++ test/index.js
@@ -1,14 +1,28 @@
-class HelloMessage extends React.Component {
-    render() {
-        return React.createElement(
-            "div",
-            null,
-            "Hello ",
-            this.props.name
-        );
+int numSquares(int n) {
+    static vector < int > dp {
+        0
+    };
+    while (dp.size() <= n) {
+        int m = dp.size(), squares = INT_MAX;
+        for (int i = 1; i * i <= m; ++i)
+            squares = min(squares, dp[m - i * i] + 1);
+        dp.push_back(squares);
     }
+    return dp[n];
 }
 
-ReactDOM.render(React.createElement(HelloMessage, {
-    name: "Jane"
-}), document.getElementById('root'));
+function numSquares(n) {
+    var dp = [];
+    while (dp.length <= n) {
+        var m = dp.length,
+            squares = Infinity;
+        for (var i = 0; i * i <= m; ++i) {
+            console.log(m, i)
+            squares = Math.min(squares, (dp[m - i * i] || 1) + 1);
+        }
+        dp.push(squares);
+
+    }
+    return dp[n];
+
+}
